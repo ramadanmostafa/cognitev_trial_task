7264b7ff9c74fd574383fe5bac12d99580bdfee7
ramadan <ramadankhalifamostafa@gmail.com>
2017-12-27 00:32:41 +0200

    create models, working

--- technical_task/models.py
+++ technical_task/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from . import COUNTRY_CODES, GENDERS
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    country_code = models.CharField(choices=COUNTRY_CODES)
+    phone_number = models.CharField(max_length=11)
+    gender = models.CharField(choices=GENDERS)
+    birthdate = models.DateField()
+    avatar = models.FileField(upload_to='')
+    email = models.EmailField(null=True, blank=True, max_length=256)
+
+
+class UserStatus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=256)
